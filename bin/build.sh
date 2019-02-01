224cdc937d59012eda16f44cd6999482908f1074
Roman <rastopyr@gmail.com>
2019-02-01 21:15:34 +0200

    fix: login to aws before push

--- bin/build.sh
+++ bin/build.sh
@@ -19,9 +19,6 @@ fi
 # Check that aws is installed
 which aws > /dev/null || { echo 'ERROR: aws-cli is not installed' ; exit 1; }
 
-# Connect into aws
-$(aws ecr get-login --no-include-email $aws_extra_flags) || { echo 'ERROR: aws ecr login failed' ; exit 1; }
-
 # Check that docker is installed and running
 which docker > /dev/null && docker ps > /dev/null || { echo 'ERROR: docker is not running' ; exit 1; }
 
@@ -31,6 +28,9 @@ echo "Building $aws_ecr_repository_url_with_tag from $dockerfile_folder/Dockerfi
 echo "docker build -t $aws_ecr_repository_url_with_tag -f $dockerfile_folder/Dockerfile $build_folder"
 # Build image
 docker build -t $aws_ecr_repository_url_with_tag -f "$dockerfile_folder/Dockerfile" $build_folder
+
+# Connect into aws
+$(aws ecr get-login --no-include-email $aws_extra_flags) || { echo 'ERROR: aws ecr login failed' ; exit 1; }
 
 # Push image
 docker push $aws_ecr_repository_url_with_tag
